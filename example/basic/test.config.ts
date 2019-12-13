9ae1ec009a7ba344ae57cb5f61708f01252ae8b6
SouthOrange <15280970040@163.com>
2019-12-13 20:36:59 +0800

    :bug: 个人信息脱敏

--- example/basic/test.config.ts
+++ example/basic/test.config.ts
@@ -10,8 +10,8 @@ export class TestConfig {
       host: "127.0.0.1",
       port: 3306,
       user: "root",
-      password: "fuyu6666",
-      database: "so_system",
+      password: "",
+      database: "",
     },
   };
 }
