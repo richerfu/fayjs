2665db5bb1b2072c3e7a7e30a023e021ddfba9ea
SouthOrange <15280970040@163.com>
2019-12-17 14:26:00 +0800

    :lipstick: 美化请求日志

--- src/plugin/RequestLog.ts
+++ src/plugin/RequestLog.ts
@@ -1,10 +1,12 @@
 import * as Koa from "koa";
 import { getFormatTime } from "./../utils/Common";
+import logger from "../utils/Logger";
+
 export async function RequestLog(ctx: Koa.Context, next: Koa.Next) {
   const reqTime = new Date();
   await next();
   const resTime = new Date();
-  console.log(
+  logger.info(
     `=>${getFormatTime(reqTime)}\t ${ctx.request.method}\t ${
       ctx.request.url
     }\t ${resTime.valueOf() - reqTime.valueOf()} ms\t`
