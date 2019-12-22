53ed5b0d14408b6e881baa11c2462c2e6e9cb573
SouthOrange <15280970040@163.com>
2019-12-22 12:10:09 +0800

    :art: 模块导出重构

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-export * from "./decorator/Decorator";
-export * from "./decorator/Context";
-export * from "./utils/Logger";
-export * from "./decorator/ParamterDecorator";
+import {
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Config,
+  Middleware,
+  Service,
+  Controller,
+  Autowired,
+} from "./decorator/Decorator";
+import { SoController, SoMiddleware, SoService } from "./decorator/Context";
+import Logger from "./utils/Logger";
+import {
+  RequestQuery,
+  RequestContext,
+  RequestBody,
+  RequestParams,
+  Body,
+} from "./decorator/ParamterDecorator";
 import SoServer from "./core";
 
 export default SoServer;
+
+export {
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Config,
+  Middleware,
+  Service,
+  Controller,
+  Autowired,
+  SoController,
+  SoMiddleware,
+  SoService,
+  Logger,
+  RequestBody,
+  RequestContext,
+  RequestParams,
+  RequestQuery,
+  Body,
+};
