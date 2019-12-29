3f3ca345c605796a352e811770156d565389de95
SouthOrange <15280970040@163.com>
2019-12-29 20:04:20 +0800

    :lock: 数据脱敏

--- example/basic/src/config/basic.config.ts
+++ example/basic/src/config/basic.config.ts
@@ -10,7 +10,7 @@ export class TestConfig {
       host: "127.0.0.1",
       port: 3306,
       user: "root",
-      password: "fuyu6666",
+      password: "Your Password",
       database: "test_sql",
     },
   };
