b2d34afbe95d2fc48cbd4d195e63bcd3867d19aa
SouthOrange <15280970040@163.com>
2020-02-28 09:59:02 +0800

    :lipstick: 优化log信息

--- src/tools/logger/index.ts
+++ src/tools/logger/index.ts
@@ -1,18 +1,30 @@
 class Log {
   public log(msg: string): void {
-    console.log(`[${new Date()}] [LOG] - ${msg}`);
+    console.log(`[${new Date().toISOString()}] [LOG] - ${msg}`);
   }
   public info(msg: string): void {
-    console.log("\x1b[32m%s\x1b[0m", `[${new Date()}] [INFO] - ${msg}`);
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      `[${new Date().toISOString()}] [INFO] - ${msg}`
+    );
   }
   public warn(msg: string): void {
-    console.log("\x1b[33m%s\x1b[0m", `[${new Date()}] [WARN] - ${msg}`);
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      `[${new Date().toISOString()}] [WARN] - ${msg}`
+    );
   }
   public debug(msg: string): void {
-    console.log("\x1b[34m%s\x1b[0m", `[${new Date()}] [DEBUG] - ${msg}`);
+    console.log(
+      "\x1b[34m%s\x1b[0m",
+      `[${new Date().toISOString()}] [DEBUG] - ${msg}`
+    );
   }
   public error(msg: string): void {
-    console.log("\x1b[31m%s\x1b[0m", `[${new Date()}] [ERROR] - ${msg}`);
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      `[${new Date().toISOString()}] [ERROR] - ${msg}`
+    );
   }
 }
 
